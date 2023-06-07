27693fa0d8700f267a110bc9ed94136cf2b7f919
sajibsaifullah <dusajibphy@gmail.com>
2023-06-07 20:59:31 +0600

    footer added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="md:min-h-[calc(100vh-300px)]">
+        <Outlet />
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,7 @@
-import Navbar from "../../Shared/Navbar/Navbar";
-
 
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>
             <h2 className="text-3xl">This is home</h2>
         </div>
     );
