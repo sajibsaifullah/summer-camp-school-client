db22c3fe6b3fabf8b8042121035c4c347b42d67b
sajibsaifullah <dusajibphy@gmail.com>
2023-06-07 10:25:33 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-center font-bold text-purple-600 mt-12">
+        Project Initialize with Tailwind & DaisyUI
+      </h1>
+      <div className="text-center mt-8">
+        <button className="btn btn-primary">Button</button>
+        <button className="btn btn-secondary">Button</button>
+        <button className="btn btn-accent">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
