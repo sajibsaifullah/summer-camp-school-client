fc925eccf3ef6d239aaeda8c8e3c673a14effb12
sajibsaifullah <dusajibphy@gmail.com>
2023-06-09 18:47:21 +0600

    social login implemented

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,6 +1,25 @@
+import { useContext } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { AuthContext } from "../../../Providers/AuthProviders/AuthProviders";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const { googleSignIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+
+        navigate(from, { replace: true });
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <>
       <div className="flex items-center pt-4 space-x-1">
@@ -10,7 +29,10 @@ const SocialLogin = () => {
         </p>
         <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
       </div>
-      <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
+      <div
+        onClick={handleGoogleSignIn}
+        className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer hover:bg-blue-300 hover:rounded-md"
+      >
         <FcGoogle size={32} />
 
         <p>Continue with Google</p>
