1ef6849837e9be7b074934e438c7d16b3d7d6195
sajibsaifullah <dusajibphy@gmail.com>
2023-06-10 09:24:46 +0600

    slider updated

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -12,31 +12,31 @@ const Slider = () => {
       >
         <SwiperSlide>
           <img src="https://i.ibb.co/GkQw2Qb/cricket.jpg" alt="" />
-          <h3 className="text-2xl text-center uppercase font-bold text-teal-600 -mt-16 ">
+          <h3 className="text-2xl text-center uppercase font-bold text-[#72ff00] -mt-16 ">
             Cricket
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://i.ibb.co/LZvSwtm/football.jpg" alt="" />
-          <h3 className="text-2xl text-center uppercase font-bold text-teal-600 -mt-16 ">
+          <h3 className="text-2xl text-center uppercase font-bold text-[#72ff00] -mt-16 ">
             Football
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://i.ibb.co/rsZK7tv/hockey.webp" alt="" />
-          <h3 className="text-2xl text-center uppercase font-bold text-teal-600 -mt-16 ">
+          <h3 className="text-2xl text-center uppercase font-bold text-[#72ff00] -mt-16 ">
             Hockey
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://i.ibb.co/Cpj7hVC/rugby.png" alt="" />
-          <h3 className="text-2xl text-center uppercase font-bold text-teal-600 -mt-16 ">
+          <h3 className="text-2xl text-center uppercase font-bold text-[#72ff00] -mt-16 ">
             Rugby
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://i.ibb.co/1Z4Z8rR/basketball.jpg" alt="" />
-          <h3 className="text-2xl text-center uppercase font-bold text-teal-600 -mt-16 ">
+          <h3 className="text-2xl text-center uppercase font-bold text-[#72ff00] -mt-16 ">
             Basketball
           </h3>
         </SwiperSlide>
